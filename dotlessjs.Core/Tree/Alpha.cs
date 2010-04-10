771349362bbd5b3e3a59bda8885cf02b80ab6d7f
James Foster <james.mdb@gmail.com>
2010-04-10 01:29:00 +0100

    Implement Alpha node.

--- dotlessjs.Core/Tree/Alpha.cs
+++ dotlessjs.Core/Tree/Alpha.cs
@@ -1,13 +1,19 @@
-﻿using System;
-using dotless.Infrastructure;
+﻿using dotless.Infrastructure;
 
 namespace dotless.Tree
 {
   public class Alpha : Node
   {
+    public Node Value { get; set; }
+
     public Alpha(Node value)
     {
-      throw new NotImplementedException();
+      Value = value;
+    }
+
+    public override string ToCSS(Env env)
+    {
+      return string.Format("alpha(opacity={0})", Value.ToCSS(env));
     }
   }
 }
