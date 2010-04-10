09a28006fff487c50d9467a21fd3b691aab6b465
James Foster <james.mdb@gmail.com>
2010-04-10 02:40:49 +0100

    Added Spec for FormatString for outputting gradient css

--- dotlessjs.Test/Specs/Functions/FormatStringFixture.cs
+++ dotlessjs.Test/Specs/Functions/FormatStringFixture.cs
@@ -42,5 +42,33 @@ namespace dotless.Tests.Specs.Functions
       AssertExpression("abc def ghi 'jkl'", "formatstring('abc {0} {1} {2}', @x, @y, @z)", variables);
     }
 
+    [Test]
+    public void FormatGradients()
+    {
+      var input = @"
+#gradients {
+  @from: #444;
+  @to: #999;
+  @ffgradient: ""-moz-linear-gradient(top, {0}, {1})"";
+  @wkgradient: ""-webkit-gradient(linear,left top,left bottom,color-stop(0, {0}),color-stop(1, {1}))"";
+  @iegradient: ""progid:DXImageTransform.Microsoft.gradient(startColorstr='{0}', endColorstr='{1}')"";
+  @ie8gradient: ""\""progid:DXImageTransform.Microsoft.gradient(startColorstr='{0}', endColorstr='{1}')\"""";
+
+  background-image: formatString(@ffgradient, @from, @to);  // FF3.6
+  background-image: formatString(@wkgradient, @from, @to);  // Saf4+, Chrome
+  filter:           formatString(@iegradient, @from, @to);  // IE6,IE7
+  -ms-filter:       formatString(@ie8gradient, @from, @to); // IE8
+}";
+
+      var expected = @"
+#gradients {
+  background-image: -moz-linear-gradient(top, #444444, #999999);
+  background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #444444),color-stop(1, #999999));
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#444444', endColorstr='#999999');
+  -ms-filter: ""progid:DXImageTransform.Microsoft.gradient(startColorstr='#444444', endColorstr='#999999')"";
+}";
+
+      AssertLess(input, expected);
+    }
   }
 }
