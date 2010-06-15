82da39ec8b880e9b4c90571fe4baf45a8f6351d3
James Foster <james.foster@excelpoint.co.uk>
2010-06-15 21:02:53 +0100

    Added overload which accepts a config object

--- src/dotless.Core/Less.cs
+++ src/dotless.Core/Less.cs
@@ -1,10 +1,17 @@
 ﻿namespace dotless.Core
 {
+    using configuration;
+
     public static class Less
     {
         public static string Parse(string less)
         {
-            return new EngineFactory().GetEngine().TransformToCss(less, null);
+            return Parse(less, DotlessConfiguration.Default);
+        }
+
+        public static string Parse(string less, DotlessConfiguration config)
+        {
+            return new EngineFactory(config).GetEngine().TransformToCss(less, null);
         }
     }
 }
