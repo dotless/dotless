43568be035a4c41b92fd9c6b7aea07f2415c9ae8
James Foster <james.foster@excelpoint.co.uk>
2010-06-23 12:33:02 +0100

    Fix compression tests

--- src/dotless.Test/CompressedSpecFixtureBase.cs
+++ src/dotless.Test/CompressedSpecFixtureBase.cs
@@ -1,10 +1,12 @@
 namespace dotless.Test.Specs.Compression
 {
     using Core.Parser.Infrastructure;
+    using NUnit.Framework;
 
     public class CompressedSpecFixtureBase : SpecFixtureBase
     {
-        public CompressedSpecFixtureBase()
+        [SetUp]
+        public void Setup()
         {
             DefaultEnv = () => new Env { Compress = true };
         }
