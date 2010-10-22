91fc74074a6ef9340ee60ef6ae65bb7b63b6e42b
U-SLO\bernie.nowel <bernie.nowel@MB177.SLO.MBO.AD>
2010-10-22 14:38:47 -0700

    Added the contrast function

---
+++ src/dotless.Core/Parser/Functions/ContrastFunction.cs
@@ -0,0 +1,19 @@
+﻿namespace dotless.Core.Parser.Functions
+{
+  using Infrastructure.Nodes;
+  using Tree;
+  using Utils;
+
+  public class ContrastFunction : HslColorFunctionBase
+  {
+    protected override Node EvalHsl(HslColor color)
+    {
+      return (color.Lightness < 0.5d) ? new Color(255d, 255d, 255d) : new Color(0d, 0d, 0d);
+    }
+
+    protected override Node EditHsl(HslColor color, Number number)
+    {
+      return null;
+    }
+  }
+}
---
+++ src/dotless.Test/Specs/Functions/ContrastFixture.cs
@@ -0,0 +1,16 @@
+﻿namespace dotless.Test.Specs.Functions
+{
+  using NUnit.Framework;
+
+  public class ContrastFixture : SpecFixtureBase
+  {
+    [Test]
+    public void TestContrast()
+    {
+      AssertExpression("white", "contrast(#000000)");
+      AssertExpression("black", "contrast(#FFFFFF)");
+
+      //AssertExpressionError("Expected Color in function 'TestContrast', found \"foo\"", 6, "contrast(\"foo\")");
+    }
+  }
+}
