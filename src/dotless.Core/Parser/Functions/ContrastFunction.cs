1df4fcd876512cca6fabbd978f0d1e07ba6fa54d
James Foster <james.foster@excelpoint.co.uk>
2011-02-09 20:32:18 +0000

    Added arguments to the Contrast function to override the light/dark color and threshold

--- src/dotless.Core/Parser/Functions/ContrastFunction.cs
+++ src/dotless.Core/Parser/Functions/ContrastFunction.cs
@@ -1,19 +1,33 @@
-﻿namespace dotless.Core.Parser.Functions
+﻿
+namespace dotless.Core.Parser.Functions
 {
-  using Infrastructure.Nodes;
-  using Tree;
-  using Utils;
+    using Infrastructure;
+    using Infrastructure.Nodes;
+    using Tree;
+    using Utils;
 
-  public class ContrastFunction : HslColorFunctionBase
-  {
-    protected override Node EvalHsl(HslColor color)
+    public class ContrastFunction : Function
     {
-      return (color.Lightness < 0.5d) ? new Color(255d, 255d, 255d) : new Color(0d, 0d, 0d);
-    }
+        protected override Node Evaluate(Env env)
+        {
+            Guard.ExpectMinArguments(1, Arguments.Count, this, Index);
+            Guard.ExpectMaxArguments(4, Arguments.Count, this, Index);
+            Guard.ExpectNode<Color>(Arguments[0], this, Index);
 
-    protected override Node EditHsl(HslColor color, Number number)
-    {
-      return null;
+            var color = HslColor.FromRgbColor((Color) Arguments[0]);
+
+            if (Arguments.Count > 1)
+              Guard.ExpectNode<Color>(Arguments[1], this, Index);
+            if (Arguments.Count > 2)
+              Guard.ExpectNode<Color>(Arguments[2], this, Index);
+            if (Arguments.Count > 3)
+              Guard.ExpectNode<Number>(Arguments[3], this, Index);
+
+            var lightColor = Arguments.Count > 1 ? (Color)Arguments[1] : new Color(255d, 255d, 255d);
+            var darkColor = Arguments.Count > 2 ? (Color)Arguments[2] : new Color(0d, 0d, 0d);
+            var threshold = Arguments.Count > 3 ? ((Number) Arguments[3]).ToNumber() : 0.5d;
+
+            return (color.Lightness < threshold) ? lightColor : darkColor;
+        }
     }
-  }
 }
--- src/dotless.Test/Specs/Functions/ContrastFixture.cs
+++ src/dotless.Test/Specs/Functions/ContrastFixture.cs
@@ -8,9 +8,44 @@
     public void TestContrast()
     {
       AssertExpression("white", "contrast(#000000)");
+      AssertExpression("white", "contrast(#797979)");
+      AssertExpression("black", "contrast(#808080)");
       AssertExpression("black", "contrast(#FFFFFF)");
+    }
 
-      //AssertExpressionError("Expected Color in function 'TestContrast', found \"foo\"", 6, "contrast(\"foo\")");
+    [Test]
+    public void TestContrastException()
+    {
+      AssertExpressionError("Expected color in function 'contrast', found \"foo\"", 0, "contrast(\"foo\")");
+    }
+
+    [Test]
+    public void OverrideLightColor()
+    {
+      AssertExpression("blue", "contrast(#000000, blue)");
+      AssertExpression("blue", "contrast(#797979, blue)");
+      AssertExpression("black", "contrast(#808080, blue)");
+      AssertExpression("black", "contrast(#FFFFFF, blue)");
+    }
+
+    [Test]
+    public void OverrideDarkColor()
+    {
+      AssertExpression("white", "contrast(#797979, white, green)");
+      AssertExpression("white", "contrast(#000000, white, green)");
+      AssertExpression("green", "contrast(#808080, white, green)");
+      AssertExpression("green", "contrast(#FFFFFF, white, green)");
+    }
+
+    [Test]
+    public void OverrideThreshold()
+    {
+      AssertExpression("white", "contrast(hsl(120, 50%, 0%), white, black, 1%)");
+      AssertExpression("white", "contrast(hsl(120, 50%, 24%), white, black, 25%)");
+      AssertExpression("black", "contrast(hsl(120, 50%, 25%), white, black, 25%)");
+      AssertExpression("white", "contrast(hsl(120, 50%, 74%), white, black, 75%)");
+      AssertExpression("black", "contrast(hsl(120, 50%, 75%), white, black, 75%)");
+      AssertExpression("black", "contrast(hsl(120, 50%, 100%), white, black, 99%)");
     }
   }
 }
