179b09d3dda4431ec7d4d73e441d4db7a0eda246
Daniel Hoelbling <tigraine@tigraine.at>
2011-02-09 18:53:08 +0100

    Fleshed out Hex function tests and introduced clamp function for clarity

--- src/dotless.Core/Parser/Functions/HexFunction.cs
+++ src/dotless.Core/Parser/Functions/HexFunction.cs
@@ -2,7 +2,7 @@ using dotless.Core.Exceptions;
 
 namespace dotless.Core.Parser.Functions
 {
-	using Infrastructure;
+    using Infrastructure;
 	using Infrastructure.Nodes;
 	using Tree;
 
@@ -13,11 +13,16 @@ namespace dotless.Core.Parser.Functions
 			if (!string.IsNullOrEmpty(number.Unit))
 				throw new ParsingException(string.Format("Expected unitless number in function 'hex', found {0}", number.ToCSS(env)), number.Index);
 
-			if (number.Value < 0) number.Value = 0;
-			if (number.Value > 255) number.Value = 255;
+		    number.Value = Clamp(number.Value, 255, 0);
 
 			return new TextNode(((int)number.Value).ToString("X2"));
-
 		}
+
+        private static double Clamp(double value, double max, double min)
+        {
+            if (value < min) value = min;
+            if (value > max) value = max;
+            return value;
+        }
 	}
 }
--- src/dotless.Test/Specs/Functions/HexFixture.cs
+++ src/dotless.Test/Specs/Functions/HexFixture.cs
@@ -11,11 +11,23 @@ namespace dotless.Test.Specs.Functions
 			AssertExpression("99", "hex(153)");
 			AssertExpression("F0", "hex(240)");
 			AssertExpression("FF", "hex(255)");
-
-			AssertExpression("00", "hex(-1)");
-			AssertExpression("FF", "hex(999)");
-
-			AssertExpressionError("Expected unitless number in function 'hex', found 5px", 4, "hex(5px)");
 		}
+
+        [Test]
+        public void ValuesBelow_0_AreInterpretedAs_0()
+        {
+            AssertExpression("00", "hex(-1)");
+        }
+
+        [Test]
+        public void ValuesAbove_255_AreInterpretedAs_FF()
+        {
+            AssertExpression("FF", "hex(999)");
+        }
+        [Test]
+        public void ThrowsExpressionError_WhenNumberPassedHasUnit()
+        {
+            AssertExpressionError("Expected unitless number in function 'hex', found 5px", 4, "hex(5px)");
+        }
 	}
 }
