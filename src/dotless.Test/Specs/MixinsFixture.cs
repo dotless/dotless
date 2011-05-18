18eaa925af3e5d05cfaee33159578f59f5877314
lukepage <luke.a.page@gmail.com>
2011-05-18 17:08:51 +0100

    Test commit adding two bug cases

--- src/dotless.Test/Specs/MixinsFixture.cs
+++ src/dotless.Test/Specs/MixinsFixture.cs
@@ -409,6 +409,41 @@ namespace dotless.Test.Specs
                 input);
         }
 
+        [Test]
+        [Ignore("Bug with named variables")]
+        public void PassAllVariablesAsNamedArgumentsWhereNoDefaultValues()
+        {
+            var input = @".clb (@a,@b) {
+  background-position: @a @b;
+}
+.cla {
+  .clb(@a:23px, @b:12px);
+}";
+            
+            var expected = @".cla {
+  background-position: 23px 12px;
+}";
+            
+            AssertLess(input, expected);
+        }
+
+        [Test]
+        [Ignore("Bug with named variables")]
+        public void SupportOnePositionArgumentOneDefaultVariableAndOneNamed()
+        {
+            var input = @".clb (@a,@b:12px,@c) {
+  background-position: @a @c;
+}
+.cla {
+  .clb(23px, @c:12px);
+}";
+
+            var expected = @".cla {
+  background-position: 23px 12px;
+}";
+              AssertLess(input, expected);
+        }
+
         [Test, Ignore("Unsupported")]
         public void ThrowsIfArumentNotFound()
         {
