84b0a53c8c5a631540dbc1a964ef2537edc95c5d
Tim Pittman <tim.pittman@snapretail.com>
2011-08-15 18:03:45 -0400

    Add proper error handling for IO errors.

--- src/dotless.Core/LessCssHttpHandler.cs
+++ src/dotless.Core/LessCssHttpHandler.cs
@@ -18,23 +18,34 @@
 
         public void ProcessRequest(HttpContext context)
         {
-            string acceptEncoding = (context.Request.Headers["Accept-Encoding"] ?? "").ToUpperInvariant();
+            try {
+                string acceptEncoding = (context.Request.Headers["Accept-Encoding"] ?? "").ToUpperInvariant();
 
-            if (acceptEncoding.Contains("GZIP"))
-            {
-                context.Response.AppendHeader("Content-Encoding", "gzip");
-                context.Response.Filter = new GZipStream(context.Response.Filter, CompressionMode.Compress);
-            }
-            else if (acceptEncoding.Contains("DEFLATE"))
-            {
-                context.Response.AppendHeader("Content-Encoding", "deflate");
-                context.Response.Filter = new DeflateStream(context.Response.Filter,
-                                                            CompressionMode.Compress);
-            }
+                if (acceptEncoding.Contains("GZIP"))
+                {
+                    context.Response.AppendHeader("Content-Encoding", "gzip");
+                    context.Response.Filter = new GZipStream(context.Response.Filter, CompressionMode.Compress);
+                }
+                else if (acceptEncoding.Contains("DEFLATE"))
+                {
+                    context.Response.AppendHeader("Content-Encoding", "deflate");
+                    context.Response.Filter = new DeflateStream(context.Response.Filter,
+                                                                CompressionMode.Compress);
+                }
 
-            var handler = Container.GetInstance<HandlerImpl>();
+                var handler = Container.GetInstance<HandlerImpl>();
             
-            handler.Execute();
+                handler.Execute();
+
+            } catch (System.IO.FileNotFoundException ex){
+                context.Response.StatusCode = 404;
+                context.Response.Write("/* File Not Found while parsing: "+ex.Message+" */");
+                context.Response.End(); 
+            } catch (System.IO.IOException ex){
+                context.Response.StatusCode = 500;
+                context.Response.Write("/* Error in less parsing: "+ex.Message+" */");
+                context.Response.End();
+            }
         }
 
         public bool IsReusable
