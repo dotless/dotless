693a78aa697a56eac779212c83d5a0c5f722e25f
Augusto Radtke <augusto.radtke@gmail.com>
2011-10-03 11:39:48 -0300

    Added support for session and application variables

--- src/dotless.Core/Parser/Tree/Variable.cs
+++ src/dotless.Core/Parser/Tree/Variable.cs
@@ -3,6 +3,7 @@ namespace dotless.Core.Parser.Tree
     using Exceptions;
     using Infrastructure;
     using Infrastructure.Nodes;
+    using System.Web;
 
     public class Variable : Node
     {
@@ -20,6 +21,18 @@ namespace dotless.Core.Parser.Tree
             {
                 var v = new Variable(name.Substring(1)).Evaluate(env);
                 name = '@' + (v is TextNode ? (v as TextNode).Value : v.ToCSS(env));
+            }
+
+            if (HttpContext.Current != null) {
+                var context = HttpContext.Current;
+
+                if (context.Session != null) {
+                    if (context.Session[name] != null)
+                        return new TextNode(context.Session[name].ToString());
+                }
+
+                if (context.Application[name] != null)
+                    return new TextNode(context.Application[name].ToString());
             }
 
             var variable = env.FindVariable(name);
