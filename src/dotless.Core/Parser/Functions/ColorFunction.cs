abe943cb4f6ea64c33086ef86c98434c2bdabb46
Luke Page <luke.a.page@gmail.com>
2012-02-26 20:56:22 +0000

    Add color function (as in less.js)

---
+++ src/dotless.Core/Parser/Functions/ColorFunction.cs
@@ -0,0 +1,18 @@
+﻿namespace dotless.Core.Parser.Functions
+{
+    using Infrastructure;
+    using Infrastructure.Nodes;
+    using Tree;
+    using Utils;
+
+    public class ColorFunction : Function
+    {
+        protected override Node Evaluate(Env env)
+        {
+            Guard.ExpectNumArguments(1, Arguments.Count, this, Index);
+            Guard.ExpectNode<TextNode>(Arguments[0], this, Index);
+
+            return new Color(((TextNode)Arguments[0]).Value.TrimStart('#'));
+        }
+    }
+}
---
+++ src/dotless.Test/Specs/Functions/ColorFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace dotless.Test.Specs.Functions
+{
+  using NUnit.Framework;
+
+  public class ColorFixture : SpecFixtureBase
+  {
+    [Test]
+    public void TestColor()
+    {
+        AssertExpression("red", @"color(""#ff0000"")");
+        AssertExpression("aqua", @"color(""#0ff"")");
+    }
+  }
+}
