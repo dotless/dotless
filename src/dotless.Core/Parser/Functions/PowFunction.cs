a76495f4a9e002fbaf09852976fb347bf6eb41db
Joel Ross <jross@rosscode.com>
2012-05-10 22:48:50 -0400

    Added exponential function. Can now do pow(2, 3) for example, and it will output 8

---
+++ src/dotless.Core/Parser/Functions/PowFunction.cs
@@ -0,0 +1,25 @@
+﻿namespace dotless.Core.Parser.Functions
+{
+    using System;
+    using System.Linq;
+    using Infrastructure;
+    using Infrastructure.Nodes;
+    using Tree;
+    using Utils;
+
+    public class PowFunction : Function
+    {
+        protected override Node Evaluate(Env env)
+        {
+            Guard.ExpectMinArguments(2, Arguments.Count, this, Index);
+            Guard.ExpectMaxArguments(2, Arguments.Count, this, Index);
+            Guard.ExpectAllNodes<Number>(Arguments, this, Index);
+
+            var first = Arguments.Cast<Number>().First();
+            var second = Arguments.Cast<Number>().ElementAt(1);
+            var value = Math.Pow(first.Value, second.Value);
+
+            return new Number(value);
+        }
+    }
+}
