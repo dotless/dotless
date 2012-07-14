4d759a2d3bf8989482e2a00febba3a6432e5bba3
Luke Page <luke.a.page@gmail.com>
2012-07-14 12:14:21 +0100

    Message is using Less.Parse for web projects to use new LessWeb.Parse. Fixes #217

---
+++ src/dotless.AspNet/LessWeb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dotless.Core.configuration;
+
+namespace dotless.Core
+{
+    public static class LessWeb
+    {
+        public static string Parse(string less, DotlessConfiguration config)
+        {
+            return new EngineFactory(config).GetEngine(new AspNetContainerFactory()).TransformToCss(less, null);
+        }
+    }
+}
--- src/dotless.Core/Less.cs
+++ src/dotless.Core/Less.cs
@@ -1,6 +1,7 @@
 ﻿namespace dotless.Core
 {
     using configuration;
+    using System;
 
     public static class Less
     {
@@ -11,6 +12,10 @@
 
         public static string Parse(string less, DotlessConfiguration config)
         {
+            if (config.Web)
+            {
+                throw new Exception("Please use dotless.Core.LessWeb.Parse for web applications. This makes sure all web features are available.");
+            }
             return new EngineFactory(config).GetEngine().TransformToCss(less, null);
         }
     }
