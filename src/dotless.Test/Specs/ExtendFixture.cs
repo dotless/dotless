1d5d0ada27c96b5c75cf808fbc0bb968b55f4cd1
Steven Pears <steven.pears@live.co.uk>
2014-06-17 18:52:49 +0100

    add nested rule code

--- src/dotless.Test/Specs/ExtendFixture.cs
+++ src/dotless.Test/Specs/ExtendFixture.cs
@@ -114,5 +114,65 @@ pre:hover,
   color: red;
 }";
         }
+
+        [Test]
+        public void ExtendNested()
+        {
+            var input = @".img-responsive(@display: block) {
+  display: @display;
+  max-width: 100%; // Part 1: Set a maximum relative to the parent
+  height: auto; // Part 2: Scale the height according to the width, otherwise you get stretching
+}
+
+.img-responsive {
+  .img-responsive();
+}
+
+.carousel-inner {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+
+  > .item {
+    display: none;
+    position: relative;
+
+    // Account for jankitude on images
+    > img,
+    > a > img {
+      &:extend(.img-responsive);
+      line-height: 1;
+    }
+  }
+}";
+
+            var extended = @".img-responsive,
+.carousel-inner > .item > img,
+.carousel-inner > .item > a > img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+
+.carousel-inner {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+}
+
+.carousel-inner > .item {
+  display: none;
+  position: relative;
+}
+
+.carousel-inner > .item > img,
+.carousel-inner > .item > a > img {
+  line-height: 1;
+}
+
+";
+            //var output = Evaluate(input, DefaultParser()).Trim().Replace("\r\n", "\n");
+            AssertLess(input,extended);
+        }
     }
 }
