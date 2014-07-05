ebb9700e60cfb95bf0f6ae135107fe977f640c50
Helen Emerson <helen@helephant.com>
2014-07-05 22:49:49 +0100

    Made the transform read from the configuration.. made it possible to override httpcontext for the bundling classes.. added the latest bootstrap to make it easy to test the console app

---
+++ src/dotless.Bundling/BundlingPathResolver.cs
@@ -0,0 +1,12 @@
+﻿using dotless.Core.Input;
+
+namespace dotless.Bundling
+{
+    class BundlingPathResolver : IPathResolver
+    {
+        public string GetFullPath(string path)
+        {
+            return path;
+        }
+    }
+}
