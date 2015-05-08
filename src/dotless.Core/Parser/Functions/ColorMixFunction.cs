05060dd54263ccc9a7ea99d5050cfb99500bbd07
Lauri Kotilainen <lauri@offbeat.fi>
2015-05-08 17:22:12 +0300

    - Take alpha channel into account when blending colors (fixes #436)

--- src/dotless.Core/Parser/Functions/ColorMixFunction.cs
+++ src/dotless.Core/Parser/Functions/ColorMixFunction.cs
@@ -1,4 +1,6 @@
-﻿namespace dotless.Core.Parser.Functions
+﻿using System;
+
+namespace dotless.Core.Parser.Functions
 {
     using Infrastructure;
     using Infrastructure.Nodes;
@@ -12,13 +14,33 @@
             Guard.ExpectNumArguments(2, Arguments.Count, this, Location);
             Guard.ExpectAllNodes<Color>(Arguments, this, Location);
 
-            var color1 = Arguments[0] as Color;
-            var color2 = Arguments[1] as Color;
+            var color1 = (Color) Arguments[0];
+            var color2 = (Color) Arguments[1];
+
+            var resultAlpha = color2.Alpha + color1.Alpha*(1 - color2.Alpha);
 
             return new Color(
-                Operate(color1.R, color2.R),
-                Operate(color1.G, color2.G), 
-                Operate(color1.B, color2.B));
+                Compose(color1, color2, resultAlpha, c => c.R),
+                Compose(color1, color2, resultAlpha, c => c.G),
+                Compose(color1, color2, resultAlpha, c => c.B),
+                resultAlpha);
+        }
+
+        /// <summary>
+        /// Color composition rules from http://www.w3.org/TR/compositing-1/
+        /// (translated from the less.js version
+        /// </summary>
+        private double Compose(Color backdrop, Color source, double ar, Func<Color, double> channel) {
+            var cb = channel(backdrop);
+            var cs = channel(source);
+            var ab = backdrop.Alpha;
+            var @as  = source.Alpha;
+            double result = Operate(cb, cs);
+            if (ar > 0)
+            {
+                result = (@as * cs + ab * (cb - @as * (cb + cs - result))) / ar;
+            }
+            return result;
         }
 
         protected abstract double Operate(double a, double b);
