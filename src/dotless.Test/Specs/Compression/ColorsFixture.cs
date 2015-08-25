dbdc2de488d15862cc5d194f47d8ccb02b2b356e
Julian Lettner <Julian.Lettner@software.dell.com>
2015-08-24 18:41:43 -0700

    Adapt tests.

--- src/dotless.Test/Specs/Compression/ColorsFixture.cs
+++ src/dotless.Test/Specs/Compression/ColorsFixture.cs
@@ -26,17 +26,17 @@ namespace dotless.Test.Specs.Compression
         [Test]
         public void Overflow()
         {
-            AssertExpression("#000", "#111111 - #444444");
-            AssertExpression("#fff", "#eee + #fff");
-            AssertExpression("#fff", "#aaa * 3");
-            AssertExpression("#0f0", "#00ee00 + #009900");
-            AssertExpression("#f00", "#ee0000 + #990000");
+            AssertExpression("#000000", "#111111 - #444444");
+            AssertExpression("#ffffff", "#eee + #fff");
+            AssertExpression("#ffffff", "#aaa * 3");
+            AssertExpression("#00ff00", "#00ee00 + #009900");
+            AssertExpression("#ff0000", "#ee0000 + #990000");
         }
 
         [Test]
         public void Gray()
         {
-            AssertExpression("#888", "rgb(136, 136, 136)");
+            AssertExpression("#888888", "rgb(136, 136, 136)");
             AssertExpression("#808080", "hsl(50, 0, 50)");
         }
     }
