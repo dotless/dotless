95a24c3ad16761b8842327e2d8541e56587c1d84
Toni Wenzel <toni.wenzel@googlemail.com>
2017-11-14 21:09:14 +0100

    Remove gems and monobuild

--- Rakefile.rb
+++
@@ -1,7 +0,0 @@
-require 'rake'
-
-task :build do
-	sh "powershell psake.ps1"
-end
-
-task :default => :build
--- monobuild/tasks/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-this=`dirname $0`
-src=$1
-out=$2
-config=$3
-
-[ -d $out ] && rm -rf $out || mkdir $out
-
