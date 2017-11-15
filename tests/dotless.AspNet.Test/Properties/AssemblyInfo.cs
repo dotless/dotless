c180137228384e242f0611fe9d6a4502ce7b5a1b
Wenzel, Toni <toni.wenzel@chg-meridian.de>
2017-11-15 13:10:14 +0100

    Improved Compatibility Tests project

---
+++ tests/dotless.AspNet.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dotless 1.5.3.0")]
+[assembly: AssemblyDescription("Dynamic CSS for .net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dotless project")]
+[assembly: AssemblyProduct("dotless")]
+[assembly: AssemblyCopyright("Copyright © $product 2010-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.5.3.0")]
+[assembly: AssemblyFileVersion("1.5.3.0")]
