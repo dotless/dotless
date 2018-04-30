305916547212c637ba8350d82a3bd5b2b676d790
Wenzel, Toni <toni.wenzel@googlemail.com>
2018-04-30 13:03:54 +0200

    Fix wrong assembly name in web.config transformation #548 Insert section at correct place #549

---
+++ src/dotless.Tools.WebTransforms/MergeBefore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace dotless.Tools.WebTransforms
+{
+    public class MergeBefore : Merge
+    {
+        protected override void Apply()
+        {
+            Apply((XmlElement)TargetNode, (XmlElement)TransformNode, Arguments);
+        }
+
+        public void Apply(XmlElement targetElement, XmlElement transformElement, IList<string> arguments)
+        {
+            var xpath = arguments.FirstOrDefault();
+
+            if (string.IsNullOrEmpty(xpath))
+                throw new ArgumentException("xpath");
+
+            var targetNode = targetElement.SelectSingleNode(xpath);
+            Apply((XmlElement)targetNode.ParentNode, transformElement);
+        }
+
+        protected override void InsertTransformElement(XmlElement targetElement, XmlElement transformElement)
+        {
+            targetElement.InsertBefore(transformElement, targetElement.ChildNodes.OfType<XmlNode>().FirstOrDefault());
+        }
+    }
+}
